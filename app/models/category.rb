31e36958c42c6d3de743b870572ec933a27f7d9d
randy-darsh <rspringer47@gmail.com>
2017-09-11 00:26:45 -0600

    fixed admin error; added some more functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
-class Category
-  has_many :gifs
+class Category < ApplicationRecord
+  has_many :gifs, dependent: :destroy
 end
--- db/migrate/20170906202842_create_gifs.rb
+++ db/migrate/20170906202842_create_gifs.rb
@@ -2,6 +2,7 @@ class CreateGifs < ActiveRecord::Migration[5.1]
   def change
     create_table :gifs do |t|
       t.string :image_url
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
